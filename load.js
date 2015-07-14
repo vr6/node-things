0de8faafb8522450ee7cac480936ec233718722f
Venkat <vpagadala@pvenkatreddy.ineda.com>
2015-07-14 22:09:28 +0530

    callback fixes

--- load.js
+++ load.js
@@ -8,14 +8,7 @@ db.collection('messages').drop();
 
 var devices = JSON.parse(fs.readFileSync(__dirname+'/app/device/data/devices.json').toString());
 var maxCount = devices.length;
-function getuser() {
-	var chars = ["a","c","d","e","h","i","k","m","o","p","r","s","t","u","v"];
-	var user = "";
-	for (var i=0; i<8; i++) {
-		user += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return user;
-}
+var dev_count = 0;
 devices.forEach(function (device) {
 	device.details = JSON.parse(fs.readFileSync(__dirname+'/app/device/data/'+device.id+'.json').toString());
 	device.platform = "android";
@@ -25,9 +18,13 @@ devices.forEach(function (device) {
 	device.phone = 900000000 + Math.floor(Math.random() * 100000000);
 	device.alerts = 2 + Math.floor(Math.random() * 10);
 	device.svcs = 5 + Math.floor(Math.random() * 6);
-	db.collection('devices').insert(device);
+	db.collection('devices').insert(device, function (err, doc) {
+		dev_count++;
+		if (dev_count == devices.length) {
+			console.log('devices loaded: ' + dev_count);
+		}
+	});
 });
-console.log('devices loaded.');
 
 var svcs = [
 	{ "name" : "Google Device Management", "image" : "devsvc/gdevsvc.png" },
@@ -39,13 +36,18 @@ var svcs = [
 	{ "name" : "Microsoft SCCM", "image" : "devsvc/sccm.jpg" },
 	{ "name" : "Microsoft Intune", "image" : "devsvc/intune.png" }
 ];
+var svcs_count = 0;
 for (var i=0; i<svcs.length; i++) {
 	svcs[i].alerts = 5 + Math.floor(Math.random() * 10);
 	svcs[i].devices = 12 + Math.floor(Math.random() * 5);
-	db.collection('devsvcs').insert ( svcs[i]);
+	db.collection('devsvcs').insert ( svcs[i], function (err, doc) {
+		svcs_count++;
+		if (svcs_count == svcs.length) {
+			console.log("devsvcs loaded: " + svcs_count);
+		}
+	});
 }
 
-console.log("devsvcs loaded");
 var msgcount = 0;
 for (var i=0; i<5; i++) {
 	db.collection('messages').insert(
@@ -55,13 +57,26 @@ for (var i=0; i<5; i++) {
 		timestamp: "2014-12-09 15:28:04",
 		device: getuser()
 	}, function (err, doc) {
-		msgcount++;
-		if (msgcount > 4) {
+		if(err) {
+			console.log("Error while inserting message.");
 			db.close();
+		} else {
+			msgcount++;
+			if (msgcount > 4) {
+				console.log("alerts loaded");
+				db.close();
+			}
 		}
 	});
 }
-console.log("alerts loaded");
+function getuser() {
+	var chars = ["a","c","d","e","h","i","k","m","o","p","r","s","t","u","v"];
+	var user = "";
+	for (var i=0; i<8; i++) {
+		user += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return user;
+}
 
 
 
